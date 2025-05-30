ad97eae88e799d01ad341f0658c93e33c2bb873e
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-05-30 18:03:36 +0300

    test deploy

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request, Header
+
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["*"]
+)
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request, query_token: Annotated[str | None, Header()] = None):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"query_token": query_token}
+    )
