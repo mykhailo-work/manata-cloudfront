c4a04cad24da0f1a3252808d8945141cbfab3873
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-05-31 04:57:01 +0300

    main

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["*"]
-)
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+
 
 @app.get("/", response_class=HTMLResponse)
-def read_root(request: Request, response: Response, cache_time: int = None):
-    response.headers["Cache-Control"] = str(cache_time) or "no-cache"
-    return templates.TemplateResponse(
-        request=request, name="index.html", context={"query_token": cache_time}
-    )
+def read_root(request: Request, cache_time: int = None):
+    response_data = {
+        "request": request,
+        "name": "index.html",
+        "context": {"query_token": cache_time},
+    }
+    
+    if cache_time is None:
+        response_data["headers"] = {
+            "Cache-Control": cache_time,
+        }
+    return templates.TemplateResponse(**response_data)
