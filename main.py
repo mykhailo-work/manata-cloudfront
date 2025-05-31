dd8e6fd7c5d290c30904230e040165a4a6a09e7f
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2025-05-31 03:12:01 +0300

    CORSMiddleware

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request, Header
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from typing import Annotated
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware, CORSMiddleware
 
 app = FastAPI()
 
@@ -12,6 +12,14 @@ templates = Jinja2Templates(directory="templates")
 app.add_middleware(
     TrustedHostMiddleware, allowed_hosts=["*"]
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request, query_token: Annotated[str | None, Header()] = None):
